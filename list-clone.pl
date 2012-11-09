d23807d84a4e25d87b55774531649422ffcc7863
Paul Fenwick <pjf@perltraining.com.au>
2012-11-09 12:47:49 +1100

    Configurable list feeds via YAML. Works.

--- list-clone.pl
+++ list-clone.pl
@@ -9,40 +9,88 @@ use WWW::Mechanize;
 use JSON::Any;
 use Data::Dumper;
 use Config::Tiny;
+use YAML::Any qw(LoadFile);
 
-my $config = Config::Tiny->read('4sq.ini');
+# Get ready for using the 4SQ API.
 
-my $TOKEN="oauth_token=$config->{auth}{token}&v=20121108";
+my $authconf = Config::Tiny->read('4sq.ini');
+
+my $TOKEN="oauth_token=$authconf->{auth}{token}&v=20121108";
 my $BASE ='https://api.foursquare.com/v2';
 
 my $mech = WWW::Mechanize->new();
 my $json = JSON::Any->new;
 
-# Scrape Jesse's list.
-# TODO: Read lists to merge from a file.
+# Read our config
 
-$mech->get( "$BASE/lists/4e4a11f02271ac3f6bd05d70?$TOKEN" );
+my $config = LoadFile("lists.yml");
 
-my $data = $json->decode( $mech->content );
+foreach my $list ( @{ $config->{lists} } ) {
+    say "Building $list->{name}";
 
-# Walk through the venues and add them.
+    my %already_on_list;
 
-foreach my $venue ( @{ $data->{response}{list}{listItems}{items} }) {
+    # Record everything on the list, so we don't add them twice.
+    foreach my $venue ( @{ get_venues_in_list($list->{id}) } ) {
+        $already_on_list{$venue->{venue}{id}}++;
+    }
 
-    my $name = $venue->{venue}{name};
-    my $id   = $venue->{venue}{id};
+    # Walk through all the lists we feed from...
+    foreach my $upstream ( @{ $list->{from} } ) {
 
-    say "Adding $name ($id)";
+        my $listinfo = get_list($upstream);
 
-    # TODO: Check for (and don't add) duplicates.
-    # TODO: Set the text for each list from upstream.
-    # TODO: Inherit tips, etc.
+        my $author   = "$listinfo->{user}{firstName} $listinfo->{user}{lastName}";
+        my $listname = $listinfo->{name};
+
+        foreach my $venue ( @{ get_venues_in_list($upstream) } ) {
+
+            my $venue_id = $venue->{venue}{id};
+
+            next if $already_on_list{$venue_id};
+
+            my $credit = "Courtesy $listname by $author";
+
+            say "* Adding $venue->{venue}{name} ($credit)";
+
+            # TODO: Inherit tips, etc.
+
+            add_to_list($list->{id}, $venue_id, $credit);
+        }
+    }
+}
+
+# TODO: Turn these into a proper OO module
+
+sub get_venues_in_list {
+    my ($list_id) = @_;
+
+    return get_list($list_id)->{listItems}{items};
+}
+
+# Get a list, and also cache it.
+sub get_list {
+    my ($list_id) = @_;
+
+    state %cache;
+
+    return $cache{$list_id} if $cache{$list_id};
+
+    $mech->get("$BASE/lists/$list_id?$TOKEN");
+
+    $cache{$list_id} = $json->decode( $mech->content )->{response}{list};
+
+    return $cache{$list_id};
+}
+
+sub add_to_list {
+    my ($list, $venue, $text) = @_;
 
     $mech->post(
-        "$BASE/lists/50933738e4b07ae1b6203788/additem?$TOKEN",
+        "$BASE/lists/$list/additem?$TOKEN",
         {
-            venueId => $id,
-            text    => "Courtesy Jesse Vincent (Wifi + Coffee + Power)",
+            venueId => $venue,
+            text    => $text,
         }
     );
 }
